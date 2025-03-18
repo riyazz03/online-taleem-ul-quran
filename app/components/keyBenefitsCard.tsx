85aa9c72dfb76972323217f876994a349e44d72a
Mohammedsalman3052004 <mdsalman3052004@gmail.com>
2025-03-19 00:01:58 +0530

    Resolved merge conflict in keyBenefitsCard.tsx

--- app/components/keyBenefitsCard.tsx
+++ app/components/keyBenefitsCard.tsx
@@ -6,7 +6,6 @@ const KeyBenefitsCard = ({ title, description, icon }: { title: string; descript
   return (
     <div className="key-benefits-card">
       <div className="key-benefits-card-icon">
-
         <Image src={icon} alt="circle" className="circle-icon" width={90} height={90} />
         <Image src={icon} alt={title} className="key-benefits-card-icon-image" width={60} height={60} />
 
